15f037ca07fce2ca7ade24775cb0bf9faf5fae1b
MarcoBasurto <macruz@tucasamas.com.mx>
2026-04-12 18:12:52 -0600

    fix cache v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'muscle-marco-v1';
+const CACHE = 'muscle-marco-v2';
 const FILES = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
@@ -15,11 +15,10 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
 
-// Notificaciones programadas
 self.addEventListener('notificationclick', e => {
   e.notification.close();
   e.waitUntil(clients.openWindow('./index.html'));
